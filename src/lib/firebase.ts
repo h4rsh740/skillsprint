54465442e925739e33561c62fc75fdd151cfafe6
Harsh Singh <166715720+h4rsh740@users.noreply.github.com>
2026-06-23 03:04:09 +0530

    chore: remove debug logs from firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,16 +2,6 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-console.log(
-  "FIREBASE_API_KEY =",
-  process.env.NEXT_PUBLIC_FIREBASE_API_KEY
-);
-
-console.log(
-  "FIREBASE_PROJECT_ID =",
-  process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
-);
-
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
